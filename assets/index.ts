656576c0b090c1c96be141c334688644d6227be1
odmoralesb <odmoralesb@gmail.com>
2020-08-06 18:34:15 -0500

    Patron de diseño singleton para agregar Ads

--- assets/index.ts
+++ assets/index.ts
@@ -1,12 +1,13 @@
 import MediaPlayer from './MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/Ads';
 
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 const playButton: HTMLElement = <HTMLButtonElement>document.querySelector('#playButton');
